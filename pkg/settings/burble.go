c19ebb2292bb92b66d05b6c2a52401438916a681
Matt Messier <orangematt@hey.com>
2024-05-12 06:25:25 -0400

    Add support for grouping jumpers by jump type

--- pkg/settings/burble.go
+++ pkg/settings/burble.go
@@ -2,7 +2,11 @@
 
 package settings
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func (s *Settings) BurbleDropzoneID() int {
 	return s.config.GetInt("burble.dzid")
@@ -19,4 +23,42 @@ func (s *Settings) OrganizerStrings() []string {
 		}
 	}
 	return o
+}
+
+type GroupByJumpType struct {
+	JumpType        string
+	ManifestHeading string
+}
+
+func (s *Settings) GroupByJumpTypes() []GroupByJumpType {
+	jumptype_groups := s.config.Get("burble.jumptype_groups")
+	groups, ok := jumptype_groups.([]interface{})
+	if !ok {
+		return nil
+	}
+
+	result := make([]GroupByJumpType, 0, len(groups))
+	for _, g := range groups {
+		gg, ggok := g.(map[string]interface{})
+		if !ggok {
+			continue
+		}
+
+		typ, tok := gg["type"].(string)
+		if !tok {
+			fmt.Fprintf(os.Stderr, "error: missing type for burble.jumptype_group\n")
+			continue
+		}
+
+		heading, hok := gg["group"].(string)
+		if !hok {
+			heading = strings.ToTitle(typ)
+		}
+		r := GroupByJumpType{
+			JumpType:        typ,
+			ManifestHeading: heading,
+		}
+		result = append(result, r)
+	}
+	return result
 }
